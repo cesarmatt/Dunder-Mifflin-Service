41bf15c2824c399321baccca231e24d2475728e6
Matheus César <cesar@Matheuss-MacBook-Pro.local>
2021-07-04 14:18:23 -0300

    SaveCustomerService up and running :)

---
+++ SaveCustomerService/src/main/kotlin/savecustomer/App.kt
@@ -0,0 +1,36 @@
+package savecustomer;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.databind.ObjectMapper
+import data.Customer
+import java.io.IOException
+
+class App : RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private var dynamoDB: AmazonDynamoDB = AmazonDynamoDBClientBuilder
+            .standard()
+            .withRegion("sa-east-1")
+            .build()
+    private var mapper: DynamoDBMapper = DynamoDBMapper(dynamoDB)
+    var objectMapper: ObjectMapper = ObjectMapper()
+
+    override fun handleRequest(input: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent {
+        val customer = objectMapper.readValue(input.body, Customer::class.java)
+        mapper.save(customer)
+
+        val response = APIGatewayProxyResponseEvent()
+
+        return try {
+            response.withStatusCode(200)
+        } catch (exception: IOException) {
+            response.withStatusCode(500)
+        }
+    }
+}
